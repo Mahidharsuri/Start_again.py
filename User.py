a22613f12c43651a4dbc0c32a30a7f28a9fc886d
Mahidharsuri <mahidharsuri07@gmail.com>
2026-01-24 19:44:21 +0530

    user

---
+++ User.py
@@ -0,0 +1,17 @@
+username = input("Enter a username :")
+
+
+while True:
+    if len(username) > 12:
+        print("You're username can't be more than 12 characters ")
+
+    elif not username.find(" ") == -1:
+        print("You're username can't contain spaces")
+
+    elif not username.isalpha():
+        print("You're username can't contain numbers ")
+
+    else:
+
+        print(f" Welcome {username}")
+        break
