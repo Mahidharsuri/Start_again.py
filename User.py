f67e2cef6e824d2fe1026456ada73fec48cfa7f4
Suri Mahidhar <mahidharsuri07@gmail.com>
2026-01-25 23:08:14 +0530

    Fix spacing check condition in username validation

--- User.py
+++ User.py
@@ -5,7 +5,7 @@ while True:
     if len(username) > 12:
         print("You're username can't be more than 12 characters ")
 
-    elif not username.find(" ") == -1:
+    elif not username.find(" ") == -1:  
         print("You're username can't contain spaces")
 
     elif not username.isalpha():
